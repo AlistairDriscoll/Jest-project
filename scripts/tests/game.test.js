b7b9d8350ffc2cde68958136ee365213fedaa4aa
AlistairDriscoll <pallythecoder@gmail.com>
2024-05-01 21:05:51 +0100

    Add testing for newGame functionality

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -3,9 +3,7 @@
  */
 
 const { beforeAll } = require("jest-circus");
-const {
-    game
-} = require("../game");
+const { game } = require("../game");
 
 
 beforeAll(() => {
@@ -29,17 +27,17 @@ describe("game object contains correct keys", () => {
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
-    test("choices contains correct ids", () => {
+    test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
 
-describe("newGame works correctly", () => {
+describe("newGame function works correctly", () => {
     beforeAll(() => {
         game.score = 42;
-        newGame();
+        newGame()
     });
-    test("should set game score to zero", () => {
+    test("newGame function should set game score to zero", () => {
         expect(game.score).toEqual(0);
-    });
+    })
 });
